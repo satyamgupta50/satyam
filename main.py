81e4e822a4d31e396474e6cb6b4741bf5fe235a5
satyam <satyamgupta52687gmail.com>
2021-03-27 17:03:23 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# print('hello')
+# print("line a \nline b")
+
+# print(78**8)
+#
+# shaaj='jamax'
+# print(shaaj)
+
+# sad=input("enter")
+# print("heko" + sad)
+
+# number_one = int(input("enter "))
+# number_second = int(input("enter "))
+# total = number_one+number_second
+# print("total is" + str(total))
+
+
